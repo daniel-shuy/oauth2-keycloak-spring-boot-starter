d901f29c950e4b99eeefc7102f5067f69e4df13f
Daniel Shuy <daniel_shuy@hotmail.com>
2023-06-08 15:04:21 +0800

    Remove explicit import of kotlin-reflect and kotlin-stdlib-jdk8

--- build.gradle.kts
+++ build.gradle.kts
@@ -128,8 +128,6 @@ dependencies {
     compileOnly("org.springframework.boot:spring-boot-starter-webflux")
 
     implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
