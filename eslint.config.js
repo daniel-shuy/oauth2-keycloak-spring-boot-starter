31ccefa9a5cba3373e073a3c0fc5f5c85101cc88
Daniel Shuy <17351764+daniel-shuy@users.noreply.github.com>
2025-09-24 22:00:24 +0800

    Use super-linter to lint/format JSON5

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,12 @@
-const { includeIgnoreFile } = require("@eslint/compat");
-const stylistic = require("@stylistic/eslint-plugin");
 const eslintPluginJsonc = require("eslint-plugin-jsonc");
 const path = require("node:path");
 
 const gitignorePath = path.resolve(__dirname, ".gitignore");
 
 module.exports = [
-  includeIgnoreFile(gitignorePath),
   ...eslintPluginJsonc.configs["flat/recommended-with-json5"],
   {
     files: ["**/*.json5"],
-    plugins: { "@stylistic": stylistic },
-    ...stylistic.configs["recommended-flat"],
     rules: {
       "jsonc/array-bracket-newline": ["error", "always"],
       "jsonc/array-element-newline": "error",
